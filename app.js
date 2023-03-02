c02bb1607a5cccaf09aa1f64cabc67ac212e788c
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2023-03-03 00:44:58 +0100

    commit

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", function (req, res) {
   res.send("FUNCIONA");
 });
 
+app.get("/nuevo", function (req, res) {
+  res.send("NUEVO FUNCIONA");
+});
+
+
 console.log("Corriendo en el puerto 5000")
 
 app.listen(process.env.PORT || 5000);
