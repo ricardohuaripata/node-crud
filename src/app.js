74d1ccce8132f37d47e8e72676e76192423b46a7
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2023-03-03 01:09:11 +0100

    commit3

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
 app.get("/", function (req, res) {
@@ -12,5 +15,7 @@ app.get("/nuevo", function (req, res) {
 
 console.log("Corriendo en el puerto 5000")
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4000);
+console.log(process.env.PORT)
+
 export default app;
