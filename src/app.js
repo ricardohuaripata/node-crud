936ed944d3d998780ac5e21005f93b40a8f0ad08
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2023-03-03 01:16:51 +0100

    commit4

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ dotenv.config();
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("FUNCIONA CORRIENDO EN EL PUERTO " + process.env.PORT);
+  res.send("FUNCIONA CORRIENDO EN EL PUERTO " + process.env.PORT + " Y EL USUARIO ES " + process.env.USUARIO);
 });
 
 app.get("/nuevo", function (req, res) {
@@ -17,5 +17,6 @@ console.log("Corriendo en el puerto 5000")
 
 app.listen(process.env.PORT || 4000);
 console.log(process.env.PORT)
+console.log(process.env.USUARIO)
 
 export default app;
